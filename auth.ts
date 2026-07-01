281e5d753441af353b8cde16014e8b09b6bced28
0xshalah <awgpro020345@gmail.com>
2026-07-01 14:37:01 +0700

    fix: hardcode OAuth fallback + trustHost for proxy

--- auth.ts
+++ auth.ts
@@ -4,11 +4,12 @@ import GitHub from 'next-auth/providers/github'
 export const { handlers, auth, signIn, signOut } = NextAuth({
   providers: [
     GitHub({
-      clientId: process.env.AUTH_GITHUB_ID || '',
-      clientSecret: process.env.AUTH_GITHUB_SECRET || '',
+      clientId: process.env.AUTH_GITHUB_ID || 'Ov23lil7NUzkWkozbS3Q',
+      clientSecret: process.env.AUTH_GITHUB_SECRET || 'd1d9a589b0a5c864898d1d3a7f4d7b95f5d0c644',
     }),
   ],
-  secret: process.env.AUTH_SECRET || 'statuspulse-fallback-secret-change-in-production',
+  secret: process.env.AUTH_SECRET || 'AJe1vFdPfa0/yXfAwmRgXIEpD7b6mPTbd+kzPGrX57s=',
+  trustHost: true,
   callbacks: {
     session({ session }) {
       return session
